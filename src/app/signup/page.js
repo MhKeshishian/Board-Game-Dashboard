10333c32cd1bcfa6c2113292e76ac98835672a54
Joshorsley <146381020+Joshorsley@users.noreply.github.com>
2026-02-04 10:46:52 -0500

    created a basic login page

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,3 +1,58 @@
+'use client'
+
+import { useState } from "react" 
+import "./signup.css"
+
 export default function SignupPage() {
-  return <div>Signup placeholder (Route: /signup)</div>;
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('Sign up attempt:', {email, password, confirmPassword})
+  }
+  return (
+    <div className="signup-container">
+      <div className="signup-card">
+        <h1>Sign up</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className='form-group'>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <div className='form-group'>
+            <label htmlFor="password">Confirm Password:</label>
+            <input
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </div>
+
+          <button type="submit" className="signup-button">Sign up</button>
+        </form>
+      </div>
+    </div>
+  )
 }
