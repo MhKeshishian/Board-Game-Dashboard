e4f38830d83dc381893ef17567696d734f26bfe0
Mher Keshishian <Mkeshishian7933@conestogac.on.ca>
2026-02-05 18:24:02 -0500

    Add save button in same line where page tab buttons are

---
+++ src/components/SaveButton/SaveButton.js
@@ -0,0 +1,18 @@
+"use client";
+
+import "./SaveButton.css";
+
+export default function SaveButton() {
+
+  function handleSave() {
+    alert("Save clicked. Backend logic goes here.");
+  }
+
+  return (
+    <div className="save-button-container">
+      <button type="button" className="save-button" onClick={handleSave}>
+        Save
+      </button>
+    </div>
+  );
+}
