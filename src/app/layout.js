a55424186e8fce5016cd61a39e303f7c928cdacb
Mher Keshishian <Mkeshishian7933@conestogac.on.ca>
2026-02-05 18:40:55 -0500

    Modify global.css and layout to make the page fit in the window.

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <Navbar />
-        {children}
+        <div className="app-wrapper">{children}</div>
       </body>
     </html>
   );
