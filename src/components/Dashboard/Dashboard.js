ab7af4a439bbbb67e30b4d88fc7e1a85fa11390c
Mher Keshishian <Mkeshishian7933@conestogac.on.ca>
2026-02-06 11:42:05 -0500

    Modify dashboard to use moduleId instead of widgetName

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -5,37 +5,37 @@ import ReactGridLayout from "react-grid-layout";
 import './Dashboard.css'
 
 export default class Dashboard extends React.PureComponent {
-  static defaultProps = {
-    cols: 12,
-    rowHeight: 100,
-    isDraggable: true,
-    isResizable: true,
-    width: 2000,
-    activePage: 0
-  };
+    static defaultProps = {
+        cols: 12,
+        rowHeight: 100,
+        isDraggable: true,
+        isResizable: true,
+        width: 2000,
+        activePage: 0
+    };
 
   constructor(props) {
     super(props);
 
     this.state = {
-      itemsByPage: [[], [], [], [], [], [], []],
-      counterByPage: [0, 0, 0, 0, 0, 0, 0]
+        itemsByPage: [[], [], [], [], [], [], []],
+        counterByPage: [0, 0, 0, 0, 0, 0, 0]
     };
 
     this.handleDrop = this.handleDrop.bind(this);
     this.handleDragOver = this.handleDragOver.bind(this);
     this.onLayoutChange = this.onLayoutChange.bind(this);
-  }
+    }
 
   handleDragOver(e) {
     e.preventDefault();
-  }
+    }
 
   handleDrop(e) {
     e.preventDefault();
 
     const pageIndex = this.props.activePage;
-    const widgetName = e.dataTransfer.getData("text/plain");
+    const moduleId = e.dataTransfer.getData("text/plain");
 
     const itemsCopy = [...this.state.itemsByPage];
     const counterCopy = [...this.state.counterByPage];
@@ -43,19 +43,19 @@ export default class Dashboard extends React.PureComponent {
     const newId = "n" + counterCopy[pageIndex];
 
     const newItem = {
-      i: newId,
-      x: (itemsCopy[pageIndex].length * 2) % this.props.cols,
-      y: Infinity,
-      w: 1,
-      h: 1,
-      widgetName: widgetName
+        i: newId,
+        x: (itemsCopy[pageIndex].length * 2) % this.props.cols,
+        y: Infinity,
+        w: 1,
+        h: 1,
+        moduleId: moduleId
     };
 
     itemsCopy[pageIndex] = itemsCopy[pageIndex].concat(newItem);
     counterCopy[pageIndex] = counterCopy[pageIndex] + 1;
 
     this.setState({ itemsByPage: itemsCopy, counterByPage: counterCopy });
-  }
+    }
 
   onRemoveItem(id) {
     const pageIndex = this.props.activePage;
@@ -64,31 +64,31 @@ export default class Dashboard extends React.PureComponent {
     itemsCopy[pageIndex] = itemsCopy[pageIndex].filter((x) => x.i !== id);
 
     this.setState({ itemsByPage: itemsCopy });
-  }
+    }
 
   onLayoutChange(layout) {
     this.props.onLayoutChange?.(layout);
-  }
+    }
 
   generateDOM() {
     const pageIndex = this.props.activePage;
     const items = this.state.itemsByPage[pageIndex];
 
     return items.map((el) =>
-      <div key={el.i} data-grid={el} className="dash-widget">
-        <button type="button" className="dash-remove" onClick={() => this.onRemoveItem(el.i)}>x</button>
-        <div className="dash-name">{el.widgetName}</div>
-      </div>
-    );
-  }
+        <div key={el.i} data-grid={el} className="dash-module">
+        <button className="dash-remove" onClick={() => this.onRemoveItem(el.i)}>x</button>
+        <div className="dash-name">{el.moduleId}</div>
+        </div>
+        );
+    }
 
   render() {
     return (
-      <div className="dashboard-area" onDragOver={this.handleDragOver} onDrop={this.handleDrop}>
+        <div className="dashboard-area" onDragOver={this.handleDragOver} onDrop={this.handleDrop}>
         <ReactGridLayout {...this.props} onLayoutChange={this.onLayoutChange}>
-          {this.generateDOM()}
+            {this.generateDOM()}
         </ReactGridLayout>
-      </div>
-    );
-  }
+        </div>
+        );
+    }
 }
